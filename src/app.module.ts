4b84d135c87f6aa9d8c9e744b8b9210356da5de1
Joo-Byungho <kjcoco13@gmail.com>
2025-03-20 15:58:22 +0900

    feat: add user module with hello query and auto-generated schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { UserModule } from './user/user.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<MercuriusDriverConfig>({
       driver: MercuriusDriver,
       graphiql: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
