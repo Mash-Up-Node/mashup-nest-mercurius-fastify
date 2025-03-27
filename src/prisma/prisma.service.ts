47daeb329c84f4b95d02b7b9cee780f7025d38d7
younha00 <ayeon0@office.skhu.ac.kr>
2025-03-27 16:56:25 +0900

    Feat: Add logger on prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,37 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>
+  implements OnModuleInit
+{
+  constructor(private readonly logger = new Logger('PrismaService')) {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'error' },
+        { emit: 'event', level: 'info' },
+        { emit: 'event', level: 'warn' },
+      ],
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+    this.$on('error', (e) => {
+      this.logger.error(e.message);
+    });
+    this.$on('info', (e) => {
+      this.logger.log(e.message);
+    });
+    this.$on('warn', (e) => {
+      this.logger.warn(e.message);
+    });
+    this.$on('query', (e) => {
+      this.logger.log(
+        `Query: ${e.query}, Params: ${e.params}, Duration: ${e.duration}ms`,
+      );
+    });
   }
 }
