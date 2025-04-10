e41ce13353b60b1f2ea99b9e40bf68f135f56f64
Joo-Byungho <kjcoco13@gmail.com>
2025-04-10 14:07:11 +0900

    feat: graphql schema 파일 생성 경로 설정 by node_env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,23 @@ import { GraphQLModule } from '@nestjs/graphql';
 
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    GraphQLModule.forRoot<MercuriusDriverConfig>({
+    GraphQLModule.forRootAsync<MercuriusDriverConfig>({
       driver: MercuriusDriver,
-      graphiql: true,
-      autoSchemaFile: '/tmp/graphql-schema.gql',
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const isDev = configService.get('NODE_ENV') !== 'production';
+        const SCHEMA_FILE_NAME = 'graphql-schema.gql';
+        return {
+          graphiql: true,
+          autoSchemaFile: (isDev ? `./src/` : '/tmp/') + SCHEMA_FILE_NAME,
+        };
+      },
     }),
     UserModule,
   ],
