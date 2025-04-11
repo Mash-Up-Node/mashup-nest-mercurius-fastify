5d3a63da61970efc1fcd505ac5c3fad6a88555b3
younha00 <ayeon0@office.skhu.ac.kr>
2025-04-11 20:53:53 +0900

    feat: auth module에 jwt module 추가 및 user module과 연결
    - .example.env에 jwt 관련 환경변수 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        privateKey: configService.get('JWT_PRIVATE_KEY'),
+        publicKey: configService.get('JWT_PUBLIC_KEY'),
+        signOptions: {
+          algorithm: 'RS256',
+          expiresIn: '1d',
+        },
+        verifyOptions: {
+          algorithms: ['RS256'],
+        },
+      }),
+    }),
+    PrismaModule,
+    ConfigModule,
+    UserModule,
+  ],
   providers: [AuthService, AuthResolver],
 })
 export class AuthModule {}
