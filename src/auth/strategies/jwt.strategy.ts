f2f92f9ada176af3803fef8091f2ad8fcabb6072
younha00 <ayeon0@office.skhu.ac.kr>
2025-04-16 01:15:40 +0900

    feat: JwtAuthGuard 구현

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,38 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthGuard, PassportStrategy } from '@nestjs/passport';
+
+import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
+
+import { UserService } from '../../user/user.service';
+
+export class JwtAuthGuard extends AuthGuard('jwt') {}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_PUBLIC_KEY'),
+    });
+  }
+
+  async validate(payload: { id: string }, done: VerifiedCallback) {
+    try {
+      const user = await this.userService.findOneById(payload.id);
+
+      if (!user) {
+        return done(null, false);
+      }
+
+      const { id, role } = user;
+      done(null, { id, role });
+    } catch (err) {
+      throw new UnauthorizedException('Error', err);
+    }
+  }
+}
