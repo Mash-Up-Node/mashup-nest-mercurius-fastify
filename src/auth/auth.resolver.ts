5f6f02f1ad2539aeadd034adb5a9313ae3d11dce
younha00 <ayeon0@office.skhu.ac.kr>
2025-04-16 22:12:15 +0900

    feat: signIn, signUp 구현

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { SignInInput, SignUpInput } from './inputs/auth.input';
+import { JwtWithUser } from './entities/auth.entity';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => JwtWithUser)
+  signIn(@Args('data') data: SignInInput) {
+    return this.authService.signIn(data);
+  }
+
+  @Mutation(() => JwtWithUser)
+  signUp(@Args('data') data: SignUpInput) {
+    return this.authService.signUp(data);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,13 +2,16 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
-import { SignInInput } from './inputs/auth.input';
+import { SignInInput, SignUpInput } from './inputs/auth.input';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../@generated/user/user.model';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly configService: ConfigService,
     private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async authenticate(input: SignInInput) {
@@ -17,23 +20,72 @@ export class AuthService {
       where: {
         email,
       },
-      select: {
-        id: true,
-        email: true,
-        password: true,
-        role: true,
-      },
     });
     if (!user) {
-      throw new BadRequestException('잘못된 로그인 정보입니다!');
+      return null;
     }
 
     const passOk: boolean = await bcrypt.compare(password, user.password);
-
     if (!passOk) {
+      return null;
+    }
+    return user;
+  }
+
+  async signUp(input: SignUpInput) {
+    const { email, password, nickname } = input;
+    const hashedPassword = await this.hashPassword(password);
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        nickname,
+        password: hashedPassword,
+      },
+    });
+    const accessToken = await this.issueToken(user, false);
+    const refreshToken = await this.issueToken(user, true);
+    return {
+      accessToken,
+      refreshToken,
+      user,
+    };
+  }
+
+  async signIn(input: SignInInput) {
+    const { email, password } = input;
+    const user = await this.authenticate({ email, password });
+    if (!user) {
       throw new BadRequestException('잘못된 로그인 정보입니다!');
     }
+    const accessToken = await this.issueToken(user, false);
+    const refreshToken = await this.issueToken(user, true);
+    return {
+      accessToken,
+      refreshToken,
+      user,
+    };
+  }
 
-    return user;
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+  }
+  async issueToken(user: User, isRefreshToken: boolean) {
+    const secret = this.configService
+      .get<string>(
+        isRefreshToken ? 'REFRESH_TOKEN_SECRET' : 'ACCESS_TOKEN_SECRET',
+      )
+      ?.replace(/\\n/g, '\n');
+    return this.jwtService.signAsync(
+      {
+        sub: user.id,
+        role: user.role,
+        type: isRefreshToken ? 'refresh' : 'access',
+      },
+      {
+        secret,
+        expiresIn: isRefreshToken ? '7d' : '15m',
+      },
+    );
   }
 }
