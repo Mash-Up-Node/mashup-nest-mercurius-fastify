639ca1f4107f592b01bf189cb94de9c60d08c25f
Joo-Byungho <kjcoco13@gmail.com>
2025-04-25 17:48:24 +0900

    feat: custom fastify 에러 작성
    - type 강제하는 법과 class 사이에서 고민하다가 런타임단에서의 안정성을 높이기 위해 class 래핑 선택

--- src/common/exception/exception.format.ts
+++ src/common/exception/exception.format.ts
@@ -1,6 +1,7 @@
 import { ExecutionResult, GraphQLFormattedError } from 'graphql';
 import { MercuriusContext } from 'mercurius';
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { CustomFastifyError } from './custom-fastify-error';
 
 export const errorFormatter = (
   execution: ExecutionResult & Required<Pick<ExecutionResult, 'errors'>>,
@@ -12,10 +13,16 @@ export const errorFormatter = (
     context.reply.log.error({ err: error }, error.message);
     const { originalError } = error;
 
+    let errorStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    let errorCode = 'INTERNAL_SERVER_ERROR';
+
     const isFastifyError = originalError?.name === 'FastifyError';
 
     if (isFastifyError) {
       statusCode = HttpStatus.BAD_REQUEST;
+      const customFastifyError = new CustomFastifyError(originalError);
+      errorCode = customFastifyError.code;
+      errorStatus = customFastifyError.statusCode;
     }
 
     /**
@@ -23,7 +30,8 @@ export const errorFormatter = (
      * CustomException 만들어서, 나머지 500 처리
      */
     if (originalError instanceof HttpException) {
-      const errorStatus = originalError.getStatus();
+      errorStatus = originalError.getStatus();
+
       if (
         [HttpStatus.UNAUTHORIZED, HttpStatus.FORBIDDEN].includes(errorStatus)
       ) {
@@ -41,6 +49,8 @@ export const errorFormatter = (
       locations: error.locations,
       path: error.path,
       extensions: {
+        errorStatus,
+        errorCode,
         ...error.extensions,
       },
     } satisfies GraphQLFormattedError;
