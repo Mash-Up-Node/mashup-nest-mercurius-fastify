1d66e1301fd5c9e48888c5e19c24fbb65f8b3f1a
Joo-Byungho <kjcoco13@gmail.com>
2025-04-30 22:05:29 +0900

    refactor: GraphQL 에러 포맷터 구조 개선

--- src/common/exception/exception.format.ts
+++ src/common/exception/exception.format.ts
@@ -1,14 +1,8 @@
-import { ExecutionResult, GraphQLFormattedError } from 'graphql';
+import { ExecutionResult, GraphQLError, GraphQLFormattedError } from 'graphql';
 import { MercuriusContext } from 'mercurius';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { CustomFastifyError } from './custom-fastify-error';
 import { BaseException } from './exception.factory';
-
-function isBaseException(
-  error: unknown,
-): error is BaseException<number, string, string> {
-  return error instanceof BaseException;
-}
 
 export const errorFormatter = (
   execution: ExecutionResult & Required<Pick<ExecutionResult, 'errors'>>,
@@ -16,55 +10,18 @@ export const errorFormatter = (
 ) => {
   let statusCode = HttpStatus.OK;
 
-  const formattedError = execution.errors.map((error) => {
-    context.reply.log.error({ err: error }, error.message);
-    const { originalError } = error;
-
-    let errorStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-    let errorCode = 'INTERNAL_SERVER_ERROR';
-
-    const isFastifyError = originalError?.name === 'FastifyError';
-
-    if (isFastifyError) {
-      statusCode = HttpStatus.BAD_REQUEST;
-      const customFastifyError = new CustomFastifyError(originalError);
-      errorCode = customFastifyError.code;
-      errorStatus = customFastifyError.statusCode;
-    }
-
-    if (isBaseException(originalError)) {
-      errorStatus = originalError.statusCode;
-      errorCode = originalError.code;
-    }
-
-    if (originalError instanceof HttpException) {
-      const originalStatus = originalError.getStatus();
-      if (
-        [HttpStatus.UNAUTHORIZED, HttpStatus.FORBIDDEN].includes(originalStatus)
-      ) {
-        statusCode = originalStatus;
-      }
-    }
-
-    if (errorStatus === HttpStatus.INTERNAL_SERVER_ERROR) {
-      statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-      /**
-       * @Todo
-       * Do sentry like
-       */
-    }
-
-    return {
-      message: error.message,
-      locations: error.locations,
-      path: error.path,
-      extensions: {
-        errorStatus,
-        errorCode,
-        ...error.extensions,
+  const formattedError = execution.errors.map((error) =>
+    formatError(error, {
+      setHttpStatus: (v) => (statusCode = v),
+      logError: () => context.reply.log.error({ err: error }, error.message),
+      captureUnexpectedException: () => {
+        /**
+         * @Todo
+         * Do sentry like
+         */
       },
-    } satisfies GraphQLFormattedError;
-  });
+    }),
+  );
 
   return {
     statusCode,
@@ -73,4 +30,92 @@ export const errorFormatter = (
       errors: formattedError,
     },
   };
+};
+
+const determineHttpStatus = (
+  error: Error | undefined,
+  errorStatus: HttpStatus,
+) => {
+  const isFastifyError = error?.name === 'FastifyError';
+
+  if (isFastifyError) {
+    return HttpStatus.BAD_REQUEST;
+  }
+
+  if (error instanceof HttpException) {
+    const originalStatus = error.getStatus();
+    if (
+      [HttpStatus.UNAUTHORIZED, HttpStatus.FORBIDDEN].includes(originalStatus)
+    ) {
+      return originalStatus;
+    }
+  }
+
+  if (errorStatus === HttpStatus.INTERNAL_SERVER_ERROR) {
+    return HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+
+  return HttpStatus.OK;
+};
+
+const isBaseException = (
+  error: unknown,
+): error is BaseException<number, string, string> => {
+  return error instanceof BaseException;
+};
+
+const determineErrorCondition = (error: Error | undefined) => {
+  const isFastifyError = error?.name === 'FastifyError';
+
+  let errorStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+  let errorCode = 'INTERNAL_SERVER_ERROR';
+
+  if (isFastifyError) {
+    const customFastifyError = new CustomFastifyError(error);
+    errorCode = customFastifyError.code;
+    errorStatus = customFastifyError.statusCode;
+  }
+
+  if (isBaseException(error)) {
+    errorStatus = error.statusCode;
+    errorCode = error.code;
+  }
+
+  return { errorStatus, errorCode };
+};
+
+interface FormatErrorOption {
+  setHttpStatus: (v: HttpStatus) => void;
+  logError: () => void;
+  captureUnexpectedException: () => void;
+}
+
+const formatError = (
+  error: GraphQLError,
+  { setHttpStatus, logError, captureUnexpectedException }: FormatErrorOption,
+) => {
+  void logError();
+
+  const { originalError } = error;
+
+  const { errorStatus, errorCode } = determineErrorCondition(originalError);
+
+  const httpStatus = determineHttpStatus(originalError, errorStatus);
+
+  void setHttpStatus(httpStatus);
+
+  if (errorStatus === HttpStatus.INTERNAL_SERVER_ERROR) {
+    void captureUnexpectedException();
+  }
+
+  return {
+    message: error.message,
+    locations: error.locations,
+    path: error.path,
+    extensions: {
+      errorStatus,
+      errorCode,
+      ...error.extensions,
+    },
+  } satisfies GraphQLFormattedError;
 };
